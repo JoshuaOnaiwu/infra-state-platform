d50d0b908967dd159076d7e615719eae0d5ab448
JoshuaOnaiwu <onaiwujoshua@gmail.com>
2026-03-20 00:33:00 +0100

    fix backend configuration - remove workspace expression

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "infra-state-platform-tf-state"
-    key            = "env/${terraform.workspace}/terraform.tfstate"
+    key            = "env/dev/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "infra-state-platform-locks"
     encrypt        = true
